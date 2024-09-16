5297ed4cdea763ae09a9585a215a176693fb2c7e
sugia <sugia@github.com>
2024-09-15 19:32:16 -0700

    update

--- src/MobileHeader.js
+++ src/MobileHeader.js
@@ -26,26 +26,29 @@ import { useNavigate } from 'react-router-dom'
 
 
 function MobileHeader() {
-    const {state, dispatch} = useContext(Context)
+    const { state, dispatch } = useContext(Context)
 
     const navigate = useNavigate()
 
     return (
         <Affix offsetTop={0}>
-            <Layout.Header style={{'background': 'white', 'width': '100%'}}>
-                <Row justify='space-between' align='middle' style={{'backgroundColor': 'white', 'width': '100%', 'height': '100%'}}>
+            <Layout.Header style={{ 'background': 'white', 'width': '100%', 'padding': '0px 0px 0px 16px' }}>
+                <Row justify='space-between' align='middle' style={{ 'backgroundColor': 'white', 'width': '100%', 'height': '100%' }}>
                     <Col onClick={() => { window.scrollTo(0, 0) }}>
                         <Row>
                             <Col>
-                                <Image width={30} height={30} preview={false} src={state.appLogo}></Image>
+                                <Image width={32} height={32} preview={false} src={state.appLogo}></Image>
                             </Col>
                             <Col>
-                                <Typography.Title level={3} style={{'color': 'black', 'marginLeft': '10px'}}>{state.appName}</Typography.Title>
+                                <Typography.Title level={4}
+                                    style={{ 'color': 'black', 'margin': '20px 0px 0px 4px' }}>
+                                    {state.appName}
+                                </Typography.Title>
                             </Col>
                         </Row>
                     </Col>
 
-                   
+
                 </Row>
             </Layout.Header>
         </Affix>
